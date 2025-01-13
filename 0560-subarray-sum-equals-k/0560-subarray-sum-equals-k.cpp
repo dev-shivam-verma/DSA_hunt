d9a117b5c17bab1596958ba11a2c682f4b8134bd
shivam <shivamvrma3012@gmail.com>
2025-01-13 15:25:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int subarr = 0;
+        int n = nums.size();
+
+        // bruteforce
+        for (int i = 0; i < n; i++){
+            for (int j = i; j < n; j++){
+                sum += nums[j];
+
+                if (sum == k) subarr++;
+            }
+        }
+
+        return subarr;
+    }
+};
