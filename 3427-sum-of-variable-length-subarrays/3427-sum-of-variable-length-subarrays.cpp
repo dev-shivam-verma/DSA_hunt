3ebb6eedf5f25dd8cb99ff691c43dffb72f8d57f
shivam <shivamvrma3012@gmail.com>
2025-01-19 12:41:17 +0530

    Time: 1 ms (27.27%), Space: 27.1 MB (36.36%) - LeetHub

---
+++ 3427-sum-of-variable-length-subarrays/3427-sum-of-variable-length-subarrays.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums) {
+        int n = nums.size();
+        int sum = 0;
+        vector<int> prefixSum;
+
+        // calculating sum of subarrays
+        for (int i = 0; i<n; i++){
+            int start = max(0, i - nums[i]);
+
+            // calculating prefix sum
+            if (i == 0){
+                prefixSum.push_back(nums[i]);
+            } else {
+                prefixSum.push_back(prefixSum[i - 1] + nums[i]);
+            }
+
+            int subSum;
+            if (start != 0){
+                subSum = prefixSum[i] - prefixSum[start - 1];
+            } else {
+                subSum = prefixSum[i]; 
+            }
+
+            sum += subSum;
+        }
+
+        return sum;
+    }
+};
