cb6da0c56c8e4d88009c2d7a912748285f2c7230
shivam <shivamvrma3012@gmail.com>
2025-02-06 13:16:04 +0530

    Time: 0 ms (100%), Space: 11.9 MB (55.31%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -3,10 +3,31 @@ public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
+        if (obstacleGrid[m - 1][n - 1] == 1) return 0;
 
         vector<vector<int>> dp(m, vector<int> (n, -1));
+        
+        // tabulation
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++){
+                int count = 0;
 
-        return recursion(m - 1, n - 1, obstacleGrid, dp);
+                if (obstacleGrid[i][j] != 1){
+                    if (i > 0) count += dp[i - 1][j];
+                    if (j > 0) count += dp[i][j - 1];
+
+                    if (i == 0 && j == 0){
+                        count = 1;
+                    }
+                }
+
+                dp[i][j] = count;
+            }
+        }
+
+        
+
+        return dp[m - 1][n - 1];
     }
 
     int recursion(int m, int n, vector<vector<int>>& grid, vector<vector<int>>& dp){
