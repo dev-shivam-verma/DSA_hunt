8d54bca0239828a8b1d394d53e7f0dcf27b96feb
shivam <shivamvrma3012@gmail.com>
2025-02-06 13:23:33 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -3,48 +3,29 @@ public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
-        if (obstacleGrid[m - 1][n - 1] == 1) return 0;
-
-        vector<vector<int>> dp(m, vector<int> (n, -1));
         
-        // tabulation
+        if (obstacleGrid[m - 1][n - 1] == 1 || obstacleGrid[0][0] == 1) return 0;
+
+        vector<int> temp(n, 0);
+        
+        // tabulation 
         for (int i = 0; i < m; i++) {
+            int prevj = 1;
             for (int j = 0; j < n; j++){
                 int count = 0;
 
                 if (obstacleGrid[i][j] != 1){
-                    if (i > 0) count += dp[i - 1][j];
-                    if (j > 0) count += dp[i][j - 1];
-
-                    if (i == 0 && j == 0){
-                        count = 1;
-                    }
+                    count += prevj;
+                    if (j > 0) count += temp[j];
                 }
 
-                dp[i][j] = count;
+                prevj = count;
+                temp[j] = count;
             }
         }
 
         
 
-        return dp[m - 1][n - 1];
-    }
-
-    int recursion(int m, int n, vector<vector<int>>& grid, vector<vector<int>>& dp){
-
-        if (grid[m][n] == 1) return 0;
-        if (m == 0 && n == 0) return 1;
-
-        int ways = 0;
-        if (m > 0 && grid[m - 1][n] != 1){
-            ways += dp[m - 1][n] != -1? dp[m - 1][n]: recursion(m - 1, n, grid, dp);
-        }
-
-        if (n > 0 && grid[m][n - 1] != 1){
-            ways += dp[m][n - 1] != -1? dp[m][n - 1]: recursion(m, n - 1, grid, dp);
-        }
-
-        dp[m][n] = ways;
-        return ways;
+        return temp[n - 1];
     }
 };
