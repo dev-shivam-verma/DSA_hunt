d2b537d9bfa38cf29e27e1d4f5010e3a0174e9f4
shivam <shivamvrma3012@gmail.com>
2025-02-06 13:24:20 +0530

    Time: 0 ms (100%), Space: 11.5 MB (89.65%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -4,7 +4,7 @@ public:
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
 
-        if (obstacleGrid[m - 1][n - 1] == 1 || obstacleGrid[0][0] == 1) return 0;
+        if (obstacleGrid[m - 1][n - 1] == 1) return 0;
 
         vector<int> temp(n, 0);
         
