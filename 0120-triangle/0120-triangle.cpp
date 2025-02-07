2a1fe90193865583b16860fa93432958aae6c4c7
shivam <shivamvrma3012@gmail.com>
2025-02-07 14:39:31 +0530

    Time: 4 ms (7.8%), Space: 12.7 MB (75.64%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -4,27 +4,26 @@ public:
         int m = triangle.size();
         int n = triangle[m - 1].size();
 
-        vector<vector<int>> dp(m, vector<int> (n, -1));
+        vector<int> dp(n, -1);
 
         // base case
         for (int j = 0; j < n; j++){
-            int i = m-1;
-            dp[i][j] = triangle[i][j];
+            dp[j] = triangle[m - 1][j];
         }
 
 
-        // tabulation
+        // tabulation with memory optemization
         for (int i = m-2; i >= 0; i--){
             for (int j = 0; j <= i; j++){
                 int curr = triangle[i][j];
-                int d = dp[i + 1][j] + curr;
-                int dg = dp[i + 1][j + 1] + curr;
+                int d = dp[j] + curr;
+                int dg = dp[j + 1] + curr;
 
-                dp[i][j] = min(d, dg);
+                dp[j] = min(d, dg);
             }
         }
 
-        return dp[0][0];
+        return dp[0];
     }
 
 
