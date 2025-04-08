bb14f306e61fad5b681318d0ca9085eca395c2fa
shivam <shivamvrma3012@gmail.com>
2025-04-08 17:43:11 +0530

    Time: 91 ms (38.17%), Space: 56.1 MB (59.83%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -1,32 +1,34 @@
 class Solution {
 public:
     int subarraysWithKDistinct(vector<int>& nums, int k) {
+        int ans = maxDist(nums, k) - maxDist(nums, k - 1);
+        return ans;
+    }
+
+    int maxDist(vector<int>& nums, int k){
         int left = 0;
-        set<int> lastOccur;
+        int ans = 0;
         unordered_map<int, int> m;
 
-        int ans = 0;
-
         for (int right = 0; right < nums.size(); right++){
-            if (m.find(nums[right]) != m.end()){
-                int idx = m[nums[right]];
-                lastOccur.erase(idx);
-            }
-            m[nums[right]] = right;
-            lastOccur.insert(right);
-
-            if (m.size() > k){
-                int lastIdx = *lastOccur.begin();
-                left = lastIdx + 1;
-                m.erase(nums[lastIdx]);
-                lastOccur.erase(lastIdx);
+            if (m.find(nums[right]) == m.end()){
+                m[nums[right]] = 1;
+            } else {
+                m[nums[right]]++;
             }
 
-            if (m.size() == k){
-                ans++;
-                int lastIdx = *lastOccur.begin();
-                ans += lastIdx - left;
+
+            while (m.size() > k){
+                if (m[nums[left]] > 1){
+                    m[nums[left]]--;
+                } else {
+                    m.erase(nums[left]);
+                }
+
+                left++;
             }
+
+            ans += right - left + 1;
         }
 
         return ans;
