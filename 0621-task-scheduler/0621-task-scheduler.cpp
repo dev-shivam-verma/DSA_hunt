8b698ab65e258027951d3c6738ab076edf0b0231
shivam <shivamvrma3012@gmail.com>
2025-04-16 18:28:42 +0530

    Time: 0 ms (100%), Space: 38.3 MB (83.29%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,36 +1,27 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        priority_queue<int> max_heap;
         vector<int> hash(26, 0);
-        list<pair<int,int>> queue;
+        int max = 0;
+        int maxCount = 0;
 
         for(char i: tasks){
             hash[i - 'A']++;
-        }
-        
-        for (int i: hash)
-            if (i > 0) max_heap.push(i);
-
-        
-        int time = 0;
-        while (!max_heap.empty() || !queue.empty()){
-            time++;
-
-            if(!max_heap.empty()){
-                int freq = max_heap.top();
-                max_heap.pop();
-
-                int coolTime = time + n;
-                if (freq > 1) queue.push_back({--freq, coolTime});
+            if(max == hash[i - 'A']) {
+                maxCount++;
             }
-
-            if (!queue.empty() && queue.front().second == time){
-                max_heap.push(queue.front().first);
-                queue.pop_front();
+            else if(max < hash[i - 'A']) {
+                max = hash[i - 'A'];
+                maxCount = 1;
             }
         }
 
-        return time;
+        int partCount = max - 1;
+        int partLength = n - (maxCount - 1);
+        int emptySlots = partCount * partLength;
+        int availableTasks = tasks.size() - max * maxCount;
+        int idles = std::max(0, emptySlots - availableTasks);        
+        
+        return tasks.size() + idles ;
     }
 };
