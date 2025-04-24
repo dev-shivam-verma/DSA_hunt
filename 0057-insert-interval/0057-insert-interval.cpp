253ae9853949af76747a78c10f92ac625b1b6165
shivam <shivamvrma3012@gmail.com>
2025-04-24 11:22:02 +0530

    Time: 0 ms (100%), Space: 21.7 MB (74.69%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -5,28 +5,20 @@ public:
         int n = intervals.size();
         int i = 0;
 
-        while (i < n && intervals[i][0] < newInterval[0]){
+        while (i < n && intervals[i][1] < newInterval[0]){
             ans.push_back(intervals[i]); i++;
         }
 
-        bool prevMerge = false;
-        int end = newInterval[1];
-        if (i != 0 && ans.back()[1] >= newInterval[0]){
-            prevMerge = true;
-            end = max(end, ans.back()[1]);
-        }
-
-        while (i < n && intervals[i][0] <= end){
-            end = max(end, intervals[i][1]);
+        while (i < n && intervals[i][0] <= newInterval[1]){
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1], intervals[i][1]);
             i++;
         }
 
-        if (prevMerge) ans.back()[1] = end;
-        else ans.push_back({newInterval[0], end});
+        ans.push_back(newInterval);
 
-        while (i < n){
-            ans.push_back(intervals[i]);
-            i++;
+        while(i < n){
+            ans.push_back(intervals[i]); i++;
         }
 
         return ans;
