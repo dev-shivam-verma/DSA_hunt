6700cb2c13092cb313e6a82d6d11965a8b725b49
shivam <shivamvrma3012@gmail.com>
2025-05-28 08:25:11 +0530

    Time: 3 ms (66.88%), Space: 17.9 MB (39.29%) - LeetHub

---
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def repeatedSubstringPattern(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        ss = (s+s)[1:-1]
+        return s in ss
+        
