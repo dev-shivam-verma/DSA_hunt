f0c441620b7265848fdbbc9e9c7f0f1f290eb885
shivam <shivamvrma3012@gmail.com>
2025-06-11 21:05:16 +0530

    Time: 16 ms (28.58%), Space: 46.6 MB (95.51%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -3,22 +3,64 @@ class Solution {
         int m = mat.length;
         int n = mat[0].length;
 
-        for (int i = 0; i < m; i++){
-            for (int j = 0; j < n; j++){
-                if (mat[i][j] == 1){
-                    mat[i][j] = Integer.MAX_VALUE;
+        int[] di = {-1,0,1,0};
+        int[] dj = {0,-1,0,1};
 
-                    for (int x = 0; x < m; x++){
-                        for (int y = 0; y < n; y++){
-                            if (mat[x][y] == 0){
-                                int distance = Math.abs(i - x) + Math.abs(j - y);
-                                mat[i][j] = Math.min(distance, mat[i][j]);
-                            }
+        int[][] visited = new int[m][n];
+        Queue<List<Integer>> que = new LinkedList<>();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 1 && visited[i][j] == 0){
+                    boolean flag = false;
+
+                    for (int x = 0; x < 4; x++) {
+                        int dyi = di[x] + i;
+                        int dyj = dj[x] + j;
+
+                        if (dyi >= 0 && dyj >= 0 && dyi < m && dyj < n && mat[dyi][dyj] == 0) {
+                            flag = true;
+                            break;
                         }
+                    }
+
+                    if (flag) {
+                        List<Integer> loc = new ArrayList<>();
+                        loc.add(i);
+                        loc.add(j);
+
+                        visited[i][j] = 1;
+
+                        que.add(loc);
                     }
                 }
             }
         }
+
+        while (!que.isEmpty()) {
+            List<Integer> loc = que.poll();
+            int i = loc.get(0);
+            int j = loc.get(1);
+
+            for (int x = 0; x < 4; x++) {
+                int dyi = di[x] + i;
+                int dyj = dj[x] + j;
+
+                if (dyi >= 0 && dyj >= 0 && dyi < m && dyj < n && mat[dyi][dyj] == 1 && visited[dyi][dyj] == 0) {
+                    mat[dyi][dyj] = mat[i][j] + 1;
+                    visited[dyi][dyj] = 1;
+                    
+                    List<Integer> dloc = new ArrayList<>();
+                    dloc.add(dyi);
+                    dloc.add(dyj);
+
+                    que.add(dloc);
+                }
+                
+            }
+        }
+
+        
 
         return mat;
     }
