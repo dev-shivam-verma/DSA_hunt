10516adcffc5bcc69d43d5383b6b66045be06414
shivam <shivamvrma3012@gmail.com>
2025-06-23 21:52:34 +0530

    Time: 25 ms (33.53%), Space: 45.3 MB (22.03%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -11,9 +11,33 @@ class Solution {
             }
         }
 
-        int ans = minCoins(coins, coins.length - 1, amount);
+        // base case (amount == 0)
+        for (int i = 0; i < coins.length; i++) {
+            dp[i][0] = 0;
+        }
+
+        for (int i = 0; i < coins.length; i++) {
+            for (int amt = 1; amt <= amount; amt++) {
+                int take = Integer.MAX_VALUE;
+                int notTake = Integer.MAX_VALUE;
+
+                // take 
+                if (coins[i] <= amt) {
+                    int result = dp[i][amt - coins[i]];
+                    if (result != Integer.MAX_VALUE) take = result + 1;
+                }
+
+
+                // not take 
+                if (i != 0) {
+                    notTake = dp[i - 1][amt];
+                }
+
+                dp[i][amt] = Integer.min(take,notTake);
+            }
+        }
         
-        return ans == Integer.MAX_VALUE ? -1: ans;
+        return dp[coins.length - 1][amount] == Integer.MAX_VALUE ? -1: dp[coins.length - 1][amount];
     }
 
     int minCoins (int[] coins, int idx, int amount) {
