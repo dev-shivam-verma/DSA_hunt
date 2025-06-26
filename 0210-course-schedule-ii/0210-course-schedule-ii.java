8618e304e3facbe8b8f704fc5e5d368adaece4de
shivam <shivamvrma3012@gmail.com>
2025-06-26 19:00:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int[] indeg = new int[numCourses];
+        List<Integer> ans = new ArrayList<>();
+        Queue<Integer> que = new LinkedList<>(); 
+        ArrayList<Integer>[] adj = new ArrayList[numCourses];
+
+        for (int i = 0; i < numCourses; i++) {
+            adj[i] = new ArrayList<Integer>();
+        }
+
+        for (int[] node: prerequisites) {
+            indeg[node[1]]++;
+            adj[node[0]].add(node[1]);
+        }
+
+        // finding nodes with indeg 0
+        for (int i = 0; i < numCourses; i++) {
+            if (indeg[i] == 0) que.add(i);
+        }
+
+        while (!que.isEmpty()) {
+            int cur = que.poll();
+
+            if (indeg[cur] == -1) return new int[0];
+
+            indeg[cur] = -1;
+            ans.add(cur);
+
+            for (int nbr: adj[cur]) {
+                indeg[nbr]--;
+                if (indeg[nbr] == 0) que.add(nbr);
+            }
+        }
+
+        Collections.reverse(ans);
+        return ans.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
